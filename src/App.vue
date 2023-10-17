17c2dd12ab05d8080631b35ca72d6a3d1b35d366
Arturo Zepeda <aaze92@gmail.com>
2023-10-16 21:59:49 -0600

    Añado routerView a app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
+import router from './router/index';
+
+
 </script>
 
 <template>
 <Header/>
+<main class=" container mx-auto mt-10">
+    <RouterView/>
+</main>
 </template>
 
 <style scoped>
