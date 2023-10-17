047a6e7b6ea46100e4a3d5179c6a4df647ec9024
Arturo Zepeda <aaze92@gmail.com>
2023-10-16 21:56:53 -0600

    Añado views de Inicio y Favorito

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import InicioViewVue from '../views/InicioView.vue';
+import FavoritosViewVue from '../views/FavoritosView.vue';
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: '/',
+      name: 'inicio',
+      component: () => InicioViewVue
+    },
+    {
+      path: '/favoritos',
+      name: 'favoritos',
+      component: () => FavoritosViewVue
+    }
   ]
 })
 
