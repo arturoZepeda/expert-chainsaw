8a180efe1c14982b79ad72530299c6431a69d75c
Arturo Zepeda <aaze92@gmail.com>
2023-10-16 22:29:22 -0600

    añado condicion para form de buscar trago

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,44 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import { computed } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+const route = useRoute();
+
+const paginaInicio = computed(() =>  route.name === 'inicio' );
+
 </script>
 <template>
-<header
-    class="bg-slate-600"
->
-    <div class="mx-auto container px-5 py-16">
-        <div class="flex justify-between items-center">
-            <div>
-                <RouterLink :to="{name: 'inicio'}">
-                    <img class="w-32 " src="/img/logo.svg" alt="Logotipo">
-                </RouterLink>
+    <header class="bg-slate-600">
+        <div class="mx-auto container px-5 py-16">
+            <div class="flex justify-between items-center">
+                <div>
+                    <RouterLink :to="{ name: 'inicio' }">
+                        <img class="w-32 " src="/img/logo.svg" alt="Logotipo">
+                    </RouterLink>
+                </div>
+                <nav class="flex gap-4">
+                    <RouterLink :to="{ name: 'inicio' }" class="text-white uppercase font-bold"
+                        active-class="text-orange-500">Inicio</RouterLink>
+                    <RouterLink :to="{ name: 'favoritos' }" class="text-white uppercase font-bold"
+                        active-class="text-orange-500">Favoritos</RouterLink>
+                </nav>
             </div>
-            <nav class="flex gap-4">
-                <RouterLink :to="{name: 'inicio'}" class="text-white uppercase font-bold" active-class="text-orange-500">Inicio</RouterLink>
-                <RouterLink :to="{name: 'favoritos'}" class="text-white uppercase font-bold" active-class="text-orange-500">Favoritos</RouterLink>
-            </nav>
-        </div>
-        <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
-            <div class="space-y-4">
-                <label for="ingrediente" class="block text-white uppercase font-extrabold text-lg">Nombre o Ingredientes</label>
-                <input id="ingrediente" type="text" class="p-3 w-full rounded-lg focus:outline-none" placeholder="Nombre o Ingrediente ej. Vodka, Tequila, etc">
-            </div>
-            <div class="space-y-4">
-                <label for="categoria" class="block text-white uppercase font-extrabold text-lg">Categoria</label>
-                <select id="categoria" class="p-3 w-full rounded-lg focus:outline-none text-lg">
-                    <option value="">-- Selecciona una categoria --</option>
+            <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6" v-if="paginaInicio">
+                <div class="space-y-4">
+                    <label for="ingrediente" class="block text-white uppercase font-extrabold text-lg">Nombre o
+                        Ingredientes</label>
+                    <input id="ingrediente" type="text" class="p-3 w-full rounded-lg focus:outline-none"
+                        placeholder="Nombre o Ingrediente ej. Vodka, Tequila, etc">
+                </div>
+                <div class="space-y-4">
+                    <label for="categoria" class="block text-white uppercase font-extrabold text-lg">Categoria</label>
+                    <select id="categoria" class="p-3 w-full rounded-lg focus:outline-none text-lg">
+                        <option value="">-- Selecciona una categoria --</option>
 
-                </select>
-            </div>
-            <input type="submit" class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase" value="Buscar recetas">
+                    </select>
+                </div>
+                <input type="submit"
+                    class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                value="Buscar recetas">
         </form>
     </div>
-</header>
-</template>    
+</header></template>    
